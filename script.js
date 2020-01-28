37af39a14268cd7f4f4fabb0c4c3767efa5ea27f
ahmed saadawi <f_22w3@hotmail.com>
2020-01-28 13:44:49 +0400

    ..

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).scroll(() => {
     var $nav = $("nav");
     if (
-      $nav.offset().top > $(window).innerHeight() - 55 &&
-      !$nav.hasClass("scrolled")
+        $nav.offset().top > $(window).innerHeight() - 55 &&
+        !$nav.hasClass("scrolled")
     ) {
-      $nav.addClass("scrolled");
+        $nav.addClass("scrolled");
     } else if (
-      $nav.offset().top < $(window).innerHeight() - 55 &&
-      $nav.hasClass("scrolled")
+        $nav.offset().top < $(window).innerHeight() - 55 &&
+        $nav.hasClass("scrolled")
     ) {
-      $nav.removeClass("scrolled");
+        $nav.removeClass("scrolled");
     }
 });

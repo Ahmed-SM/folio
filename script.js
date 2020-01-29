1a3c85fccfceb3e8069037292e66c51e36ac2792
ahmed saadawi <f_22w3@hotmail.com>
2020-01-29 16:22:29 +0400

    Colors Change

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
     var yScrollPos = $(this).scrollTop();
-    var scrollPos= $(window).innerHeight() - 55;
-    if(yScrollPos > scrollPos-900){
-        $("#footer").addClass("footer-anim");
-    }else{
-        $("#footer").removeClass("footer-anim");
+    var scrollPos = $(window).innerHeight() - 55;
+    if (yScrollPos > scrollPos - 900) {
+        $("#footer").addClass("footer-anim-white");
+        if (yScrollPos > scrollPos + 1650) {
+            $("#footer").removeClass("footer-anim-white");
+            $("#footer").addClass("footer-anim");
+        } else {
+            $("#footer").removeClass("footer-anim");
+        }
+    } else {
+        $("#footer").removeClass("footer-anim-white");
     }
-    if(yScrollPos > scrollPos) {
-    var $nav = $("#nav");
-    $nav.addClass("scrolled");
-    }else if (
-        yScrollPos  < $(window).innerHeight() - 55 &&
+
+    if (yScrollPos > scrollPos) {
+        var $nav = $("#nav");
+        $nav.addClass("scrolled");
+    } else if (
+        yScrollPos < $(window).innerHeight() - 55 &&
         $("#nav").hasClass("scrolled")
     ) {
-    $("#nav").removeClass("scrolled");
+        $("#nav").removeClass("scrolled");
     }
 });

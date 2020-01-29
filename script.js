461cd383b8e6097070bf7531b1ba101abec3b5a3
ahmed saadawi <f_22w3@hotmail.com>
2020-01-29 15:43:58 +0400

    ..

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 $(window).scroll(function() {
     var yScrollPos = $(this).scrollTop();
     var scrollPos= $(window).innerHeight() - 55;
+    if(yScrollPos > scrollPos-900){
+        $("#footer").addClass("footer-anim");
+    }else{
+        $("#footer").removeClass("footer-anim");
+    }
     if(yScrollPos > scrollPos) {
     var $nav = $("#nav");
     $nav.addClass("scrolled");
